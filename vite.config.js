3525def9b8da77c8e43f6c66137b176e892b9a21
kamel996 <kamelnassar2828@gmail.com>
2023-04-21 18:33:11 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "build",
+    assetsDir: "assets",
+    sourcemap: true,
+    minify: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "src/index.js"),
+      },
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
