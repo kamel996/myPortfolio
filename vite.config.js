7e04fb4353feed51a286a4cbdff5d96940aca437
kamel996 <kamelnassar2828@gmail.com>
2023-04-21 18:36:20 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build",
-    assetsDir: "assets",
-    sourcemap: true,
-    minify: true,
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, "src/index.js"),
+        main: "./src/index.js",
+        "react-tilt": "./node_modules/react-tilt/tilt.js",
       },
-    },
-  },
-  server: {
-    port: 3000,
-  },
-  optimizeDeps: {
-    include: ["react", "react-dom"],
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
     },
   },
 });
