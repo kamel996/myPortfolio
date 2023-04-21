625a885cd57d2e0db52e3b1a746023eb5f9ca3ac
kamel996 <kamelnassar2828@gmail.com>
2023-04-21 18:42:49 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       input: {
-        main: "./src/index.js",
-        "react-tilt": "./node_modules/react-tilt/tilt.js",
-      },
-    },
-  },
+        main: './src/index.js',
+        'react-tilt': './node_modules/react-tilt/src/tilt.js'
+      }
+    }
+  }
 });
